b166a093ad7a2468397c93b8ae6461245d78521f
Daniel Schneider <daschne8@gmail.com>
2019-03-31 16:00:56 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,43 @@
+class Board
+  attr_accessor :cells
+  def initialize
+    reset!
+  end
+
+  def reset!
+    @cells = Array.new(9," ")
+  end
+
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+
+  def position(user_input)
+    @cells[user_input.to_i-1]
+  end
+
+  def full?
+    !@cells.include?(" ")
+  end
+
+  def turn_count
+    9 - @cells.count(" ")
+  end
+
+  def taken?(position)
+    @cells[position.to_i - 1] != " "
+  end
+
+  def valid_move?(user_input)
+    user_input.to_i <=9 && user_input.to_i >=1 && !taken?(user_input)
+  end
+
+  def update(position, player)
+    @cells[position.to_i - 1] = player.token
+  end
+
+end
---
+++ lib/players/human.rb
@@ -0,0 +1,15 @@
+module Players
+  class Human < Player
+
+    def move(board)
+      input = gets#.chomp
+      if board.valid_move?(input)
+        board.update(input,self)
+        return input
+      else
+        return "invalid"
+      end
+    end
+
+  end
+end
